cfc21d8c871d129c6ff5d859b8e9f68f0297c958
Abdullah Khan <abdkhann77@gmail.com>
2026-07-23 18:49:17 +0500

    solved task 2.3 figuring out what is set data structure

---
+++ task2.3.py
@@ -0,0 +1,8 @@
+nums = [4, 2, 7, 2, 9, 4, 4, 1]
+unique_nums = set(nums) # set is used to remove the duplicates from the list
+print(f"The unique numbers in the list are: {unique_nums}")
+if 7 in unique_nums:
+    print("7 is present in the list.")
+else:
+    print("7 is not present in the list.")
+    
